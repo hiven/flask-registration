d41618755baab55f00d8ae32e10f25076e69fc9c
hiven <mail@hiven.com>
2023-03-13 16:44:09 -0400

    Update config.py

--- config.py
+++ config.py
@@ -18,69 +18,9 @@ class BaseConfig(object):
 
     # mail accounts
     MAIL_DEFAULT_SENDER = 'from@example.com'
-
-
-class DevelopmentConfig(BaseConfig):
-    """Development configuration."""
     DEBUG = True
     WTF_CSRF_ENABLED = False
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'dev.sqlite')
     DEBUG_TB_ENABLED = True
 
 
-class TestingConfig(BaseConfig):
-    """Testing configuration."""
-    LOGIN_DISABLED=False
-    TESTING = True
-    DEBUG = False
-    BCRYPT_LOG_ROUNDS = 1
-    WTF_CSRF_ENABLED = False
-    DEBUG_TB_ENABLED = False
-    SQLALCHEMY_DATABASE_URI = 'sqlite://'
-
-
-class ProductionConfig(BaseConfig):
-    """Production configuration."""
-    DEBUG = False
-    DEBUG_TB_ENABLED = False
-
-    SECRET_KEY = None
-    SECURITY_PASSWORD_SALT = None
-
-    STRIPE_SECRET_KEY = None
-    STRIPE_PUBLISHABLE_KEY = None
-
-    SQLALCHEMY_DATABASE_URI = None
-
-    # production config takes precedence over env variables
-
-    # production config file at ./project/config/production.cfg
-    config_path = os.path.join(basedir, 'config', 'production.cfg')
-
-    # if config file exists, read it:
-    if os.path.isfile(config_path):
-        config = configparser.ConfigParser()
-
-        with open(config_path) as configfile:
-            config.readfp(configfile)
-
-        SECRET_KEY = config.get('keys', 'SECRET_KEY')
-        SECURITY_PASSWORD_SALT = config.get('keys', 'SECRET_KEY')
-
-        # mail settings
-        MAIL_SERVER = config.get('mail', 'MAIL_SERVER')
-        MAIL_PORT = config.getint('mail', 'MAIL_PORT')
-        MAIL_USE_TLS = config.getboolean('mail', 'MAIL_USE_TLS')
-        MAIL_USE_SSL = config.getboolean('mail', 'MAIL_USE_SSL')
-
-        # mail authentication and sender
-        MAIL_USERNAME = config.get('mail', 'MAIL_USERNAME')
-        MAIL_PASSWORD = config.get('mail', 'MAIL_PASSWORD')
-        MAIL_DEFAULT_SENDER = config.get('mail', 'MAIL_DEFAULT_SENDER')
-
-        # database URI
-        SQLALCHEMY_DATABASE_URI = config.get('db', 'SQLALCHEMY_DATABASE_URI')
-
-        # stripe keys
-        STRIPE_SECRET_KEY = config.get('stripe', 'STRIPE_SECRET_KEY')
-        STRIPE_PUBLISHABLE_KEY = config.get('stripe', 'STRIPE_PUBLISHABLE_KEY')
