5384b0a9166de37a0fecfcc1e091da379441b2e0
hiven <mail@hiven.com>
2023-03-13 14:17:58 -0400

    Update views.py

--- project/main/views.py
+++ project/main/views.py
@@ -1,25 +1,7 @@
-# project/main/views.py
-
-
-#################
-#### imports ####
-#################
-
 from flask import render_template
-from flask import Blueprint
 from flask_login import current_user
 from flask_login import login_required
 
-################
-#### config ####
-################
-
-main_blueprint = Blueprint('main', __name__,)
-
-
-################
-#### routes ####
-################
 
 @main_blueprint.route('/')
 def home():
